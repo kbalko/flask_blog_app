0aaae53347300d2a7ddee53123758f354b9c1116
kbalko <balkrzys@users.noreply.github.com>
2020-08-13 15:41:09 +0200

    fix

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
-import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    DATABASE_URL = os.environ.get('DATABASE_URL') 
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "test"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POST_PER_PAGE = 10
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
